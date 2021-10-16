4d45be83d75468a24ae06a9ef1ce30a0a866f285
NataliHTML <nataly07081992>
2021-10-16 18:59:21 +0300

    added page sort

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function () {
 
+	// $('.filter__input').on('click', function () {
+	// 	$('.filter__menu').removeClass('filter__menu--active');
+	// 	$(this).addClass('filter__menu--active');
+	// 	$('.filter__menu').removeClass('filter__menu--active');
+	// });
+
+
+	document.querySelector(".filter__input").addEventListener("click", function (e) {
+		document.querySelector(".filter__menu").classList.toggle("filter__menu--active");
+	});
 
 });
